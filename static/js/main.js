08af3dc35b30512d5420d9e6022cb7fb69e7cd9b
Tim Sergeant <tgsergeant@gmail.com>
2015-01-12 19:48:53 +1100

    gags

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,24 @@
+MEMES = [
+  {
+    type: "img",
+    url: "/static/img/curran.jpg"
+  },
+]
+
 $(document).ready(function() {
-	console.log("Hello!");
+  ({
+    selectMeme: function () {
+      var meme_count = MEMES.length;
+      var i = Math.floor(Math.random() * meme_count);
+      var meme = MEMES[i];
+      console.log(meme);
+      if (meme.type == "img") {
+        $(".meme-inner").html("<img src='" + meme.url + "' />");
+      }
+    },
+    setup: function() {
+      this.selectMeme();
+    }
+  }).setup();
+
 });
